cf946aff14473667c3d45652b2c0f7d1518509d3
Hmmambk1994 <bhmmam@gmail.com>
2020-02-19 07:20:41 +0200

    I have faced couple of issues so I treid to face them all in once
    for next time : just make a psudo code to help you not get lost

--- script.js
+++ script.js
@@ -1,9 +1,74 @@
 let buttons=document.querySelectorAll(".button")
 
-buttons.forEach(button => button.addEventListener("click", changeColor))
+buttons.forEach(button => button.addEventListener("click", buttonClicked))
 
-function changeColor(){
-    let button=this;
+// the array we create to make the player try memorize it
+let baseArray=[];
+
+// edit this later
+let level=3;
+round();
+
+
+/********************
+ **** functions ***** 
+ ********************/
+
+function round(){
+    fillBaseArray();
+
+    buttons.forEach(button => {
+        // light buttons up for user so he can memeorize them
+    })
+}
+
+
+
+function buttonClicked() {
+    changeBodyColor(this);
+
+}
+
+function changeButtonsColor() {
+    
+}
+
+function changeBodyColor(buttonNumber){
+    let button=buttonNumber;
     let cssColor= getComputedStyle(button);
     document.body.style.background = cssColor.background;
 }
+
+function fillBaseArray(){
+    baseArray=[];
+    for( let i =0;i<level;i++){
+        let randomNum=Math.floor(Math.random()*level);
+        baseArray.push(randomNum+1);
+    }
+}
+
+
+
+// // the array that we get from user input 
+// let playerArray=[];
+
+
+// (function round(){
+//     fillBaseArray();
+//     console.log(baseArray);
+//     playerArray=[];
+
+//     level++;
+// })()
+
+
+
+
+
+// function checkArrays(baseArr, playerArr){
+//     for( let i=0;i<baseArr.length;i++){
+//         if(baseArr[i]!==playerArr[i]) return false;
+//     }
+//     return true;
+// }
+
