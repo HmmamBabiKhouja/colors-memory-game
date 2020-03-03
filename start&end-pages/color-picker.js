d5fd469f865b1992dc9a8390388742ab0011d5bc
Hmmambk1994 <bhmmam@gmail.com>
2020-03-03 21:45:13 +0200

    more document color picker

--- start&end-pages/color-picker.js
+++ start&end-pages/color-picker.js
@@ -1,5 +1,6 @@
 let randomColorPicker=Math.floor(Math.random()*989)+10;
 // for colors contrast the 10 added so the background wouldn't get close to my logo color
+// which is hsl(0,100%, 72%);
 
 let color=`hsl(${randomColorPicker},100%, 72%)`
 
