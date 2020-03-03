9aebaf93f363cdff0590485859a731c899a9a154
Hmmambk1994 <bhmmam@gmail.com>
2020-03-03 21:43:52 +0200

    color picker avoids getting a close color to logo

--- start&end-pages/color-picker.js
+++ start&end-pages/color-picker.js
@@ -1,4 +1,5 @@
-let randomColorPicker=Math.floor(Math.random()*999);
+let randomColorPicker=Math.floor(Math.random()*989)+10;
+// for colors contrast the 10 added so the background wouldn't get close to my logo color
 
 let color=`hsl(${randomColorPicker},100%, 72%)`
 
